4297108f803f00233c8e5ecbac664c55b3cb9949
unknown <saikatm3121@gmail.com>
2023-10-17 02:50:49 +0600

    updated al all

--- js/mian.js
+++ js/mian.js
@@ -1,23 +1,18 @@
-var typed = new Typed('#element', {
+;(function($){
+    $(document).ready(function(){ 
+      $('.single-image').hover(function(){
+        var status = $(this).children('.hidden').css('visibility');
+        if(status== 'hidden'){
+          $(this).children('.hidden').css('visibility','visible');
+        }else{
+          $('.hidden').css('visibility','hidden');
+
+        }
+      })
+
+  })})(jQuery);
+
+  var typed = new Typed('#element', {
     strings: ['Wordpress Theme Developer', 'WordPress Plugin Developer','WordPress SEO Expert and Specialist'],
     typeSpeed: 100,
   });
-
-var image_url = [
-]
-
-
-  var salary = [
-    ["ABC", 24, 18000],
-    ["EFG", 30, 30000],
-    ["IJK", 28, 41000],
-    ["EFG", 31, 28000],
- ];
- salary.forEach(myfunction);
-
- function myfunction(item){
-  item.forEach(all_item);
- }
-function all_item(item){
-console.log(item);
-}
