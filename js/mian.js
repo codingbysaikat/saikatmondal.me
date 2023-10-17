c42d93c51c3fca1f972006acb7ac51e64426d336
unknown <saikatm3121@gmail.com>
2023-10-17 18:18:24 +0600

    updated

--- js/mian.js
+++ js/mian.js
@@ -2,16 +2,24 @@
     $(document).ready(function(){ 
       $('.single-image').hover(function(){
         var status = $(this).children('.hidden').css('visibility');
+        var image_url = $(this).children('img').attr('src');
         if(status== 'hidden'){
           $(this).children('.hidden').css('visibility','visible');
+          $('.modal-content > .single-image img').attr('src',image_url);
         }else{
           $('.hidden').css('visibility','hidden');
 
         }
-      })
+      });
+      function is_screen(element){
+
+        
+        return false;     
+      }
+
 
   })})(jQuery);
-
+// type js plugin code here
   var typed = new Typed('#element', {
     strings: ['Wordpress Theme Developer', 'WordPress Plugin Developer','WordPress SEO Expert and Specialist'],
     typeSpeed: 100,
